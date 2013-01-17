cd9bd38687b78ea9dbf31a7c40cb46328b24fa1e
cheeky bastard <alkaholic@runbox.com>
2013-01-17 16:28:20 +1100

    basic polls, 1st draft

--- django_angularjs/urls.py
+++ django_angularjs/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from polls import views
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_angularjs_rest.views.home', name='home'),
     # url(r'^django_angularjs_rest/', include('django_angularjs_rest.foo.urls')),
+     url(r'^polls/', include('polls.urls')),
      url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
      url(r'^admin/', include(admin.site.urls)),
 )
